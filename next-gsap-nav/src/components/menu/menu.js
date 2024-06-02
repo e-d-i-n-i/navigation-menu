96775c0fdcbc2dd5418aa02b0eb0c9f3c6e2d5aa
Edini Amare Getachew <122861398+e-d-i-n-i@users.noreply.github.com>
2024-06-02 23:54:31 +0300

    gsap + styling

--- next-gsap-nav/src/components/menu/menu.js
+++ next-gsap-nav/src/components/menu/menu.js
@@ -4,6 +4,8 @@ import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 
 import "./menu.css";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const menuLinks = [
   { path: "/", label: "Home" },
@@ -20,6 +22,38 @@ const Menu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const tl = useRef();
+
+  useGSAP(
+    () => {
+      gsap.set(".menu-link-item-holder", { y: 75 });
+
+      tl.current = gsap
+        .timeline({ paused: true })
+        .to(".menu-overlay", {
+          duration: 1.25,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "power4.inOut",
+        })
+        .to(".menu-link-item-holder", {
+          y: 0,
+          duration: 1,
+          stagger: 0.1,
+          ease: "power4.inOut",
+          delay: -0.75,
+        });
+    },
+    { scope: container }
+  );
+
+  useEffect(() => {
+    if (isMenuOpen) {
+      tl.current.play();
+    } else {
+      tl.current.reverse();
+    }
+  }, [isMenuOpen]);
+
   return (
     <div className="menu-container" ref={container}>
       <div className="menu-bar">
@@ -30,52 +64,74 @@ const Menu = () => {
           <p>Menu</p>
         </div>
       </div>
-      {isMenuOpen && (
-        <div className="menu-overlay">
-          <div className="menu-overlay-bar">
-            <div className="menu-logo">
-              <Link href="/">Codegrid</Link>
-            </div>
-            <div className="menu-close" onClick={toggleMenu}>
-              <p>Close</p>
-            </div>
+      <div className="menu-overlay">
+        <div className="menu-overlay-bar">
+          <div className="menu-logo">
+            <Link href="/">Codegrid</Link>
           </div>
-          <div className="menu-close-icon" onClick={toggleMenu}>
-            <p>&#x2715;</p>
-          </div>
-          <div className="menu-copy">
-            <div className="menu-links">
-              {menuLinks.map((link, index) => (
-                <div className="menu-link-item" key={index}>
-                  <div className="menu-link-item-holder" onClick={toggleMenu}>
-                    <Link href={link.path} className="menu-link">
-                      {link.label}
-                    </Link>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="menu-info">
-              <div className="menu-info-col">
-                <a href="#">X &#8599;</a>
-                <a href="#">Instagram &#8599;</a>
-                <a href="#">LinkedIn &#8599;</a>
-                <a href="#">Behance &#8599;</a>
-                <a href="#">Dribbble &#8599;</a>
-              </div>
-              <div className="menu-info-col">
-                <p>info@codegrid.com</p>
-                <p>2342 232 343</p>
-              </div>
-            </div>
-          </div>
-          <div className="menu-preview">
-            <p>View Showreel</p>
+          <div className="menu-close" onClick={toggleMenu}>
+            <p>Close</p>
           </div>
         </div>
-      )}
+        <div className="menu-close-icon" onClick={toggleMenu}>
+          <p>&#x2715;</p>
+        </div>
+        <div className="menu-copy">
+          <div className="menu-links">
+            {menuLinks.map((link, index) => (
+              <div className="menu-link-item" key={index}>
+                <div className="menu-link-item-holder" onClick={toggleMenu}>
+                  <Link href={link.path} className="menu-link">
+                    {link.label}
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="menu-info">
+            <div className="menu-info-col">
+              <a href="#">X &#8599;</a>
+              <a href="#">Instagram &#8599;</a>
+              <a href="#">LinkedIn &#8599;</a>
+              <a href="#">Behance &#8599;</a>
+              <a href="#">Dribbble &#8599;</a>
+            </div>
+            <div className="menu-info-col">
+              <p>info@codegrid.com</p>
+              <p>2342 232 343</p>
+            </div>
+          </div>
+        </div>
+        <div className="menu-preview">
+          <p>View Showreel</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Menu;
+{
+  /* 
+        
+      
+      
+        
+         
+        </div>
+       
+          
+        </div>
+        <div className="menu-copy">
+          
+            
+          </div>
+          
+            
+          </div>
+        </div>
+       
+         
+        </div>
+      </div> */
+}
